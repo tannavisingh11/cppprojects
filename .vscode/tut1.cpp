12cbea41e850b7e197bb3aa12d9fab5bf2a5e576
tannavisingh11 <tannavisingh11@gmail.com>
2026-07-30 14:05:16 +0530

    initial commit

---
+++ .vscode/tut1.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+// This program was created by Tannavi
+
+int main(){
+    cout<<"Hello World";
+    return 0;
+}
---
+++ oopsreference.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int a=10;
+	int &ref=a;
+	a=100;
+	cout<<"a="<<a<<endl;
+	cout<<"ref="<<ref<<endl;
+	
+	return 0;
+	
+}
